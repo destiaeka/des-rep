fc9d26a1eef6cf53a348084e5ba6e54702edabc1
destiaeka <163701104+destiaeka@users.noreply.github.com>
2024-06-18 16:58:13 +0700

    Add files via upload

---
+++ PAS.py
@@ -0,0 +1,32 @@
+import random
+
+def main():
+  print("Yukkk, mulai permainan tebak angka")
+  print("===START===")
+  # Tentukan batas atas dan bawah untuk tebakan
+  angka_maksimum = 100
+  angka_minimum = 1
+
+
+  # Memilih angka acak menggunakan fungsi 'randint' yang disediakan oleh modul 'random'
+  angka_rahasia = random.randint(angka_minimum, angka_maksimum)
+
+  # Memberikan kesempatan kepada pemain untuk menebak
+  tebakan = 0
+  while tebakan != angka_rahasia:
+    tebakan = int(input(f"Masukkan tebakan Anda antara {angka_minimum} dan {angka_maksimum}: "))
+
+    if tebakan < angka_rahasia:
+      print("Yah, tebakanmu terlalu rendah")
+    elif tebakan > angka_rahasia:
+      print("Aduh, ketinggian nih")
+    else:
+      print("Nah, ini dia angka rahasianya!!")
+  # Output apabila angka tebakan betul
+  print(f"Jadi angka rahasianya tuh {angka_rahasia}")
+
+if __name__ == "__main__":
+  main()
+
+
+
